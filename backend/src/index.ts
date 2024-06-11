d545d51eb5639743724f8be9deb1c474aa296806
Muhammad Zulfikar <muhammad.zulfikar@podifi.ai>
2024-06-11 12:36:35 +0700

    [feat]:mongoose and frontend init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,16 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import "dotenv/config";
-import healthcheckRoutes from './routes/healthcheck'
+import healthcheckRoutes from './routes/healthcheck';
+import mongoose from 'mongoose';
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 
 app.use("", healthcheckRoutes)
+mongoose.connect(process.env.MONGODB_URI as string);
 
 app.listen(process.env.PORT, () => {
     console.log("server started")
--- backend/src/routes/healthcheck.ts
+++ backend/src/routes/healthcheck.ts
@@ -1,9 +1,30 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
 router.get("/healthcheck", async (req: Request, res: Response) => {
-    res.json({ message: "Server is running" });
+    let server_status = "Server is running";
+    let db = mongoose.connection;
+    let mongo_status;
+    const state = mongoose.connection.readyState;
+    if (state === 0) {
+        mongo_status = 'MongoDB is disconnected'
+    } else if (state === 1) {
+        mongo_status = 'MongoDB is connected'
+    } else if (state === 2) {
+        mongo_status = 'MongoDB is connecting'
+    } else if (state === 3) {
+        mongo_status = 'MongoDB is disconnecting'
+    } else {
+        mongo_status = 'MongoDB connection state is unknown'
+    }
+    res.json({
+        message: {
+            "Server status": server_status,
+            "MongoDB status": mongo_status
+        }
+    });
 });
 
 export default router;
