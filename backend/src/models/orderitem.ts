e3390fb7cc2f938d10b7da1460e266bafff949b9
Muhammad Zulfikar <muhammad.zulfikar@podifi.ai>
2024-06-12 11:51:33 +0700

    [feat]: entities, types init

---
+++ backend/src/models/orderitem.ts
@@ -0,0 +1,14 @@
+import { Model, Schema, model } from 'mongoose';
+import { MenuItemTypes } from './menuitem';
+
+export interface OrderItemTypes extends Document {
+    menuItem_id: MenuItemTypes;
+    quantity: number;
+}
+
+export const orderItemSchema: Schema = new Schema({
+    menuItem_id: { type: Schema.Types.ObjectId, required: true },
+    quantity: { type: Number, default: 0 },
+});
+
+export const OrderItemModel: Model<OrderItemTypes> = model<OrderItemTypes>('Order Item', orderItemSchema);
--- backend/src/shared/types.ts
+++ backend/src/shared/types.ts
@@ -8,3 +8,77 @@ export class NotificationDTO {
     }
 
 }
+
+export class MenuDTO {
+    type: string;
+    menuItems: MenuItemDto[];
+
+    constructor(type: string, menuItems: MenuItemDto[]) {
+        this.type = type;
+        this.menuItems = menuItems;
+    }
+
+}
+
+export class MenuItemDto {
+    menuItemId: string;
+    name: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+
+    constructor(menuItemId: string, name: string, description: string, price: number, imageUrl: string) {
+        this.menuItemId = menuItemId;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+}
+
+export class OrderDTO {
+    totalOrderPrice: number;
+    orderItems: OrderItemDTO[];
+
+    constructor(orderItems: OrderItemDTO[]) {
+        this.totalOrderPrice = this.getTotalOrderPrice(orderItems);
+        this.orderItems = orderItems;
+    };
+
+    getTotalOrderPrice(orderItems: OrderItemDTO[]): number {
+        let sum = 0;
+        orderItems.forEach(orderItem => {
+            sum += orderItem.itemTotal;
+        })
+        return sum;
+    };
+
+}
+
+export class OrderItemDTO {
+    menuItemId: string;
+    menuItemName: string;
+    menuItemDescription: string;
+    menuItemPrice: number;
+    menuItemImageUrl: string;
+    quantity: number;
+    itemTotal: number;
+
+    constructor(menuItemId: string, quantity: number) {
+        let menuItem = this.getMenuItem();
+        this.menuItemId = menuItemId;
+        this.menuItemName = menuItem.name;
+        this.menuItemDescription = menuItem.description;
+        this.menuItemPrice = menuItem.price;
+        this.menuItemImageUrl = menuItem.imageUrl;
+        this.quantity = quantity;
+        this.itemTotal = quantity * menuItem.price;
+    }
+
+    getMenuItem(): MenuItemDto {
+        throw new Error("Method not implemented.");
+    }
+
+
+}
