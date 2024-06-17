491285619acb1cacada8803ad2ac899b17a80231
Muhammad Zulfikar <muhammad.zulfikar@podifi.ai>
2024-06-17 17:26:23 +0700

    [feat] some linter and some npm i

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,33 +1,37 @@
-import { FunctionComponent, memo, useMemo, type CSSProperties } from "react";
+import { FunctionComponent, memo, useMemo, type CSSProperties } from 'react';
 
 type ButtonType = {
-  buttonText?: string;
+    buttonText?: string;
 
-  /** Style props */
-  buttonMinWidth?: CSSProperties["minWidth"];
+    /** Style props */
+    buttonMinWidth?: CSSProperties['minWidth'];
 
-  /** Action props */
-  onButtonContainerClick?: () => void;
+    /** Action props */
+    onButtonContainerClick?: () => void;
 };
 
 const Button: FunctionComponent<ButtonType> = memo(
-  ({ buttonText = "View Orders", onButtonContainerClick, buttonMinWidth }) => {
-    const buttonStyle: CSSProperties = useMemo(() => {
-      return {
-        minWidth: buttonMinWidth,
-      };
-    }, [buttonMinWidth]);
+    ({
+        buttonText = 'View Orders',
+        onButtonContainerClick,
+        buttonMinWidth,
+    }) => {
+        const buttonStyle: CSSProperties = useMemo(() => {
+            return {
+                minWidth: buttonMinWidth,
+            };
+        }, [buttonMinWidth]);
 
-    return (
-      <div
-        className="bg-gray-100 flex flex-col items-start justify-start py-2.5 px-[15px] text-sm text-white font-aleo border-[1px] border-solid border-black cursor-pointer text-center"
-        onClick={onButtonContainerClick}
-        style={buttonStyle}
-      >
-        <div className="relative w-full">{buttonText}</div>
-      </div>
-    );
-  },
+        return (
+            <div
+                className="bg-gray-100 flex flex-col items-start justify-start py-2.5 px-[15px] text-sm text-white font-aleo border-[1px] border-solid border-black cursor-pointer text-center"
+                onClick={onButtonContainerClick}
+                style={buttonStyle}
+            >
+                <div className="relative w-full">{buttonText}</div>
+            </div>
+        );
+    }
 );
 
 export default Button;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,32 +1,32 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import createCache from "@emotion/cache";
-import { CacheProvider } from "@emotion/react";
-import "./global.css";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+import './global.css';
 
 const chakraTheme = extendTheme({
-  styles: { global: { img: { maxWidth: "unset" } } },
+    styles: { global: { img: { maxWidth: 'unset' } } },
 });
 const emotionCache = createCache({
-  key: "emotion-cache",
-  prepend: true,
+    key: 'emotion-cache',
+    prepend: true,
 });
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container!);
 
 root.render(
-  <BrowserRouter>
-    <CacheProvider value={emotionCache}>
-      <ChakraProvider theme={chakraTheme}>
-        <App />
-      </ChakraProvider>
-    </CacheProvider>
-  </BrowserRouter>,
+    <BrowserRouter>
+        <CacheProvider value={emotionCache}>
+            <ChakraProvider theme={chakraTheme}>
+                <App />
+            </ChakraProvider>
+        </CacheProvider>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
